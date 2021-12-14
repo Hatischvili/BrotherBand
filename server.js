2f5b9f657c1fce6e5e1ec771ade29946bacad17a
Clarice-Hatischvili <claricehatischvili@gmail.com>
2021-12-13 21:34:23 -0300

    PELO AMOR DE DEUS

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require ("./src/app");
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running. Port: ${port}`));
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv-safe").config();
+require("dotenv").config();
 
 
 const db = require("./database/mongoConfig");
